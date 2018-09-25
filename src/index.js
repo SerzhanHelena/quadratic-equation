639c8be258e6e36c416f387b7dff4b967d8dce6f
SerzhanHelena <serzhan.helen@gmail.com>
2018-09-25 12:30:31 +0300

    task4

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+
+  var numbers = equation.match(/(\-?|\+\s|\-\s)[0-9]+/g).map(item => Number(item.replace(/\s/g, '')));
+  
+  var a = numbers[0];
+  var b = numbers[2];
+  var c = numbers[3];
+
+  var D = (b * b) - 4 * a * c;
+  var x1 = (-b + Math.sqrt(D)) / (2 * a);
+  var x2 = (-b - Math.sqrt(D)) / (2 * a);
+
+  return [
+      Math.round(x1),
+      Math.round(x2)
+  ].sort((a, b) => a - b)
+
 }
